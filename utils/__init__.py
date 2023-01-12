891fb3cdf84eefd4dd675ef0fdc1a69b1d3aeae0
shu421 <shugox1@gmail.com>
2023-01-12 11:13:43 +0900

    update: path

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from utils.exp_manage import *
-from utils.utils import *
-from utils.model_selection import (
+from kagglib.utils.exp_manage import *
+from kagglib.utils.utils import *
+from kagglib.utils.model_selection import (
     get_StratifiedKFold_list,
     get_StratifiedGroupKFold_list,
 )
